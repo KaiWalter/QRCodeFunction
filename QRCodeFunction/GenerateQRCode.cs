46d6ba6b76078c08e5558ca7963b652a23b202e8
Kai Walter <kai.walter@zeiss.com>
2019-03-07 18:43:42 +0000

    implement dpi and size parameters

--- QRCodeFunction/GenerateQRCode.cs
+++ QRCodeFunction/GenerateQRCode.cs
@@ -18,14 +18,38 @@ namespace QRCodeFunction
         {
             log.LogInformation("GenerateQRCode received a request.");
 
+            // process input parameters
             string data = req.Query["data"];
+            string size = req.Query["size"];
+            string dpi = req.Query["dpi"];
 
+            int sizeValue = 50;
+            if (!string.IsNullOrEmpty(size))
+            {
+                int.TryParse(size, out sizeValue);
+            }
+
+            int dpiValue = 600;
+            if (!string.IsNullOrEmpty(dpi))
+            {
+                int.TryParse(dpi, out dpiValue);
+            }
+
+            // render QR code into bitmap
             if (!string.IsNullOrEmpty(data))
             {
                 var writer = new QRCodeWriter();
                 var pixelData = writer.encode(data, ZXing.BarcodeFormat.QR_CODE, 0, 0);
-                var renderer = new ZXing.CoreCompat.Rendering.BitmapRenderer();
-                var bitmap = renderer.Render(pixelData, ZXing.BarcodeFormat.QR_CODE, data);
+                var renderer = new ZXing.CoreCompat.Rendering.BitmapRenderer()
+                {
+                    DpiX = dpiValue,
+                    DpiY = dpiValue
+                };
+                var bitmap = renderer.Render(pixelData, ZXing.BarcodeFormat.QR_CODE, data, new ZXing.Common.EncodingOptions()
+                {
+                    Height = sizeValue,
+                    Width = sizeValue
+                });
 
                 return new FileContentResult(ImageToByteArray(bitmap), "image/jpeg");
             }
